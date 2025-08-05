c30e8e35a0081f3e7cf135fc15f2675fd697c99b
Subhajit Kar <subhajitkar19@gmail.com>
2025-08-05 13:27:15 +0300

    updates

--- src/SSG.jl
+++ src/SSG.jl
@@ -103,6 +103,90 @@
 # end # module SSG
 
 
+# module SSG
+
+#     using MPI
+#     using PencilArrays
+#     using PencilFFTs
+#     using FFTW
+#     using LinearAlgebra
+#     using Statistics
+#     using Printf
+#     using Random
+#     using Dates
+#     using JLD2
+
+#     # Constants
+#     const FT = Float64
+
+#     # Include files in dependency order
+#     include("params.jl")           # No dependencies
+#     include("domain.jl")           # Depends on params (FT constant)
+#     include("fields.jl")           # Depends on domain
+#     include("transforms.jl")       # Depends on domain, fields
+#     include("utils.jl")            # Depends on domain, fields, transforms
+#     include("coarse_domain.jl")    # Depends on domain
+#     include("poisson.jl")          # Depends on all above
+#     include("filter.jl")           # Depends on domain, fields
+#     include("timestep.jl")         # Depends on fields, domain, transforms
+#     include("nonlinear.jl")        # Depends on fields, domain, transforms
+#     include("output.jl")           # Depends on fields, domain
+#     include("setICs.jl")           # Depends on all above
+
+#     # Export main functions
+
+#     # domain.jl exports
+#     export Domain, make_domain, gridpoints, gridpoints_2d
+
+#     # fields.jl exports  
+#     export Fields, allocate_fields, zero_fields!, copy_field!, field_stats
+
+#     # params.jl exports
+#     export Params, validate_params, has_diffusion, has_hyperdiffusion, has_filter
+
+#     # transforms.jl exports
+#     export rfft!, irfft!, dealias!, ddx!, ddy!, ddz!, d2dxdy!, d2dz2!,
+#         jacobian!, laplacian_h!, laplacian_3d!, divergence_3d!, gradient_h!,
+#         compute_vorticity_z!, parsevalsum, parsevalsum2
+
+#     # utils.jl exports
+#     export jacobian, jacobianh, advection_term!, compute_energy, compute_enstrophy,
+#         compute_total_buoyancy, compute_buoyancy_variance, compute_cfl_number,
+#         norm_field, inner_product, create_real_field, create_spectral_field,
+#         enhanced_field_stats
+
+#     # timestep.jl exports
+#     export TimeScheme, TimeParams, TimeState, SemiGeostrophicProblem,
+#         timestep!, integrate!, step_until!, stepforward!, run!,
+#         AB2_LowStorage, RK3, RK3_LowStorage, DiagnosticTimeSeries, update_diagnostics!
+
+#     # nonlinear.jl exports
+#     export compute_tendency!, compute_geostrophic_velocities!
+
+#     # poisson.jl exports
+#     export solve_ssg_equation, solve_monge_ampere_fields!, compute_ma_residual_fields!,
+#         SSGLevel, SSGMultigridSolver
+
+#     # output.jl exports
+#     export OutputFrequency, OutputManager, save_simulation_state_full,
+#         save_snapshot, save_spectral_snapshot, process_all_outputs!,
+#         load_simulation_state_full
+
+#     # filter.jl exports
+#     export AbstractSpectralFilter, ExponentialFilter, HyperviscosityFilter,
+#         CutoffFilter, CesaroFilter, apply_spectral_filter!, create_filter
+
+#     # setICs.jl exports
+#     export set_b!, set_φ!
+
+#     # coarse_domain.jl exports
+#     export create_coarse_domain
+
+# end # module SSG
+
+
+
+# src/SSG.jl - Main module file
 module SSG
 
     using MPI
@@ -113,81 +197,70 @@ module SSG
     using Statistics
     using Printf
     using Random
-    using Dates
     using JLD2
 
     # Constants
     const FT = Float64
 
-    # Include files in dependency order
-    include("params.jl")           # No dependencies
-    include("domain.jl")           # Depends on params (FT constant)
-    include("fields.jl")           # Depends on domain
-    include("transforms.jl")       # Depends on domain, fields
-    include("utils.jl")            # Depends on domain, fields, transforms
-    include("coarse_domain.jl")    # Depends on domain
-    include("poisson.jl")          # Depends on all above
-    include("filter.jl")           # Depends on domain, fields
-    include("timestep.jl")         # Depends on fields, domain, transforms
-    include("nonlinear.jl")        # Depends on fields, domain, transforms
-    include("output.jl")           # Depends on fields, domain
-    include("setICs.jl")           # Depends on all above
+    # Include files in order
+    include("utils.jl")
+    include("domain.jl")
+    include("fields.jl")
+    include("transforms.jl")
+    include("params.jl")
+    include("filter.jl")
+    include("poisson.jl")
+    include("timestep.jl")
+    include("output.jl")
+    include("nonlinear.jl")
+    
+    # Optional files (if they exist)
+    #isfile("nonlinear.jl") && include("nonlinear.jl")
 
-    # Export main functions
+    # Exports by file:
 
-    # domain.jl exports
-    export Domain, make_domain, gridpoints, gridpoints_2d
+    # utils.jl
+    export @ensuresamegrid, twothirds_mask, create_real_field, create_spectral_field,
+           copy_field!, zero_field!, norm_field, inner_product
 
-    # fields.jl exports  
-    export Fields, allocate_fields, zero_fields!, copy_field!, field_stats
+    # domain.jl  
+    export Domain, make_domain, gridpoints, gridpoints_2d, dealias!
 
-    # params.jl exports
+    # fields.jl
+    export Fields, allocate_fields, zero_fields!, field_stats, enhanced_field_stats
+
+    # transforms.jl
+    export rfft!, irfft!, ddx!, ddy!, ddz!, laplacian_h!, jacobian!, jacobianh,
+           parsevalsum, parsevalsum2, compute_energy, compute_enstrophy
+
+    # params.jl
     export Params, validate_params, has_diffusion, has_hyperdiffusion, has_filter
 
-    # transforms.jl exports
-    export rfft!, irfft!, dealias!, ddx!, ddy!, ddz!, d2dxdy!, d2dz2!,
-        jacobian!, laplacian_h!, laplacian_3d!, divergence_3d!, gradient_h!,
-        compute_vorticity_z!, parsevalsum, parsevalsum2
+    # filter.jl
+    export ExponentialFilter, HyperviscosityFilter, CutoffFilter, 
+           apply_spectral_filter!, create_filter
 
-    # utils.jl exports
-    export jacobian, jacobianh, advection_term!, compute_energy, compute_enstrophy,
-        compute_total_buoyancy, compute_buoyancy_variance, compute_cfl_number,
-        norm_field, inner_product, create_real_field, create_spectral_field,
-        enhanced_field_stats
+    # poisson.jl
+    export solve_ssg_equation, SSGLevel, solve_monge_ampere_fields!, 
+           solve_poisson_simple
 
-    # timestep.jl exports
-    export TimeScheme, TimeParams, TimeState, SemiGeostrophicProblem,
-        timestep!, integrate!, step_until!, stepforward!, run!,
-        AB2_LowStorage, RK3, RK3_LowStorage, DiagnosticTimeSeries, update_diagnostics!
+    # timestep.jl
+    export TimeParams, TimeState, SemiGeostrophicProblem, AB2_LowStorage, RK3,
+           timestep!, step_until!, run!
 
-    # nonlinear.jl exports
+    # output.jl
+    export save_simulation_state_full, load_simulation_state_full, OutputManager
+
+    # nonlinear.jl (if exists)
+    #if isdefined(SSG, :compute_tendency!)
     export compute_tendency!, compute_geostrophic_velocities!
-
-    # poisson.jl exports
-    export solve_ssg_equation, solve_monge_ampere_fields!, compute_ma_residual_fields!,
-        SSGLevel, SSGMultigridSolver
-
-    # output.jl exports
-    export OutputFrequency, OutputManager, save_simulation_state_full,
-        save_snapshot, save_spectral_snapshot, process_all_outputs!,
-        load_simulation_state_full
-
-    # filter.jl exports
-    export AbstractSpectralFilter, ExponentialFilter, HyperviscosityFilter,
-        CutoffFilter, CesaroFilter, apply_spectral_filter!, create_filter
-
-    # setICs.jl exports
-    export set_b!, set_φ!
-
-    # coarse_domain.jl exports
-    export create_coarse_domain
+    
+    #end
 
 end # module SSG
 
-# ============================================================================
-# File structure that should be created:
-# ============================================================================
 
+# File structure that should be:
 #=
 src/
 ├── SSG.jl                # Main module (above)
--- src/fields.jl
+++ src/fields.jl
@@ -69,7 +69,6 @@ function allocate_fields(domain::Domain{T}) where T
     φ    = PencilArray(domain.pr, zeros(T, local_size(domain.pr)))
     u    = PencilArray(domain.pr, zeros(T, local_size(domain.pr)))
     v    = PencilArray(domain.pr, zeros(T, local_size(domain.pr)))
-    ω_z  = PencilArray(domain.pr, zeros(T, local_size(domain.pr)))
     R    = PencilArray(domain.pr, zeros(T, local_size(domain.pr)))
     tmp  = PencilArray(domain.pr, zeros(T, local_size(domain.pr)))
     tmp2 = PencilArray(domain.pr, zeros(T, local_size(domain.pr)))
@@ -85,7 +84,7 @@ function allocate_fields(domain::Domain{T}) where T
     tmpc2 = PencilArray(domain.pc, zeros(Complex{T}, local_size(domain.pc)))
     
     return Fields{T, typeof(b), typeof(bhat)}(
-        bₛ, φₛ, φ, u, v, ω_z, 
+        bₛ, φₛ, φ, u, v, 
         φ_mg, b_mg, R, 
         tmp, tmp2, tmp3, 
         bhat, φhat, 
@@ -98,28 +97,29 @@ end
 Set all fields to zero.
 """
 function zero_fields!(fields::Fields)
-    fields.bₛ    .= 0.0
-    fields.φₛ    .= 0.0
+    fields.bₛ.data    .= 0.0
+    fields.φₛ.data    .= 0.0
 
-    fields.φ     .= 0.0
-    fields.u     .= 0.0
-    fields.v     .= 0.0
-
-    fields.φ_mg  .= 0.0
-    fields.b_mg  .= 0.0
-    fields.R     .= 0.0
-
-    fields.tmp   .= 0.0
-    fields.tmp2  .= 0.0
-    fields.tmp3  .= 0.0
-
-    fields.bhat  .= 0.0
-    fields.φhat  .= 0.0
+    fields.φ.data     .= 0.0
+    fields.u.data     .= 0.0
+    fields.v.data     .= 0.0
     
-    fields.tmpc  .= 0.0
-    fields.tmpc2 .= 0.0
+    fields.φ_mg.data  .= 0.0
+    fields.b_mg.data  .= 0.0
+    
+    fields.R.data     .= 0.0
+    
+    fields.tmp.data   .= 0.0
+    fields.tmp2.data  .= 0.0
+    fields.tmp3.data  .= 0.0
+    
+    fields.bhat.data  .= 0.0
+    fields.φhat.data  .= 0.0
+    
+    fields.tmpc.data  .= 0.0
+    fields.tmpc2.data .= 0.0
 
-    return fields
+    #return fields
 end
 
 
@@ -149,54 +149,85 @@ Copy one field to another (must be same type and size).
 """
 function copy_field!(dest, src)
     @ensuresamegrid(dest, src)
-    dest .= src
+    dest.data .= src.data
     return dest
 end
 
 
 """
-    field_stats(field) -> (mean, std, min, max)
-
-Compute basic statistics for a field.
+    zero_field!(field)
+Set all values in a PencilArray to zero.
 """
-function field_stats(field)
+function zero_field!(field::PencilArray)
+    field.data .= 0
+    #return field
+end
+
+
+"""
+    field_stats(field) -> NamedTuple
+
+Compute basic statistics for a single field.
+"""
+function field_stats(field::PencilArray{T}) where T
+    field_data = field.data
+    
+    # Local statistics
+    local_mean  = mean(field_data)
+    local_min   = minimum(field_data)
+    local_max   = maximum(field_data)
+    local_count = length(field_data)
+    
+    # Global statistics via MPI (if available)
+    if MPI.Initialized() && MPI.Comm_size(field.pencil.comm) > 1
+        global_sum   = MPI.Allreduce(local_mean * local_count, MPI.SUM, field.pencil.comm)
+        global_count = MPI.Allreduce(local_count, MPI.SUM, field.pencil.comm)
+        global_mean  = global_sum / global_count
+        
+        global_min = MPI.Allreduce(local_min, MPI.MIN, field.pencil.comm)
+        global_max = MPI.Allreduce(local_max, MPI.MAX, field.pencil.comm)
+        
+        # Global variance
+        local_var_contrib = sum((field_data .- global_mean).^2)
+        global_var = MPI.Allreduce(local_var_contrib, MPI.SUM, field.pencil.comm) / global_count
+        global_std = sqrt(global_var)
+    else
+        # Single process case
+        global_mean = local_mean
+        global_min = local_min
+        global_max = local_max
+        global_std = std(field_data)
+        global_count = local_count
+    end
+    
+    return (
+        mean = global_mean,
+        std = global_std,
+        min = global_min,
+        max = global_max,
+        count = global_count
+    )
+end
+
+"""
+    enhanced_field_stats(fields::Fields) -> Dict
+
+Compute enhanced statistics for all fields in Fields structure.
+"""
+function enhanced_field_stats(fields::Fields)
     stats = Dict{Symbol, NamedTuple}()
+    
+    # Get all field names that are PencilArrays with real element types
     for field_name in fieldnames(typeof(fields))
         field = getfield(fields, field_name)
         if isa(field, PencilArray) && eltype(field) <: Real
-            field_data = field.data
-            
-            # Local statistics
-            local_mean = mean(field_data)
-            local_min   = minimum(field_data)
-            local_max   = maximum(field_data)
-            local_count = length(field_data)
-            
-            # Global statistics via MPI
-            global_sum   = MPI.Allreduce(local_mean * local_count, MPI.SUM, field.pencil.comm)
-            global_count = MPI.Allreduce(local_count, MPI.SUM, field.pencil.comm)
-            global_mean  = global_sum / global_count
-            
-            global_min = MPI.Allreduce(local_min, MPI.MIN, field.pencil.comm)
-            global_max = MPI.Allreduce(local_max, MPI.MAX, field.pencil.comm)
-            
-            # Global variance
-            local_var_contrib = sum((field_data .- global_mean).^2)
-            global_var = MPI.Allreduce(local_var_contrib, MPI.SUM, field.pencil.comm) / global_count
-            global_std = sqrt(global_var)
-            
-            stats[field_name] = (
-                mean = global_mean,
-                std = global_std,
-                min = global_min,
-                max = global_max,
-                count = global_count
-            )
+            stats[field_name] = field_stats(field)
         end
     end
     
     return stats
 end
+
 
 """
     Base.show(io::IO, fields::Fields)
