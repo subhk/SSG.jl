d46354bd79db26f60a24fb5999ecc1232957a519
Subhajit Kar <subhajitkar19@gmail.com>
2025-08-09 17:34:28 +0300

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,11 +5,11 @@ println("Running SSG.jl Test Suite")
 println("=" ^ 50)
 
 # Run Poisson solver tests
-println("\n🧮 POISSON SOLVER TESTS")
+println("\n POISSON SOLVER TESTS")
 println("-" ^ 30)
 include("test_poisson.jl")
 
-println("\n⏰ TIME INTEGRATION TESTS") 
+println("\n TIME INTEGRATION TESTS") 
 println("-" ^ 30)
 include("test_timestep.jl")
 
